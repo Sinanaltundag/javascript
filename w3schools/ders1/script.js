ddbd705549bbe26b9a91722ba886a2445c0bcdfd
peykani <peykani@gmail.com>
2022-01-18 17:06:49 +0300

    bazı string metodları

---
+++ w3schools/ders1/script.js
@@ -0,0 +1,27 @@
+/* function kapatma() {
+    alert('nereye gidiyorsun');
+}
+var letters = "escape sequences.. \b	Backspace   \f	Form Feed   \n <br>	New Line   \r	Carriage Return   \t	Horizontal \
+Tabulator   \v	Vertical Tabulator"
+function escseq() {
+   document.getElementsByTagName("h1")[0].innerHTML=letters ;
+} */
+
+/* function escseq() {
+    let str = "What are you doing?"
+    document.getElementsByTagName("h1")[0].innerHTML=str.slice(-12) ;
+ } */
+let str = "What are you doing?"
+ function escseq2() {
+    
+    document.getElementsByTagName("h2")[0].innerHTML=str.substring(5,9) ;
+ }
+ function escseq() {
+    
+    document.getElementsByTagName("h1")[0].innerHTML=str.substr(13,2) ;
+ }
+ function escseq3() {
+    
+    document.getElementsByTagName("h2")[1].innerHTML=str.replace("are", "were") ;
+ }
+ 
--- w3schools/script.js
+++
@@ -1,8 +0,0 @@
-function kapatma() {
-    alert('nereye gidiyorsun');
-}
-var letters = "escape sequences.. \b	Backspace   \f	Form Feed   \n <br>	New Line   \r	Carriage Return   \t	Horizontal \
-Tabulator   \v	Vertical Tabulator"
-function escseq() {
-   document.getElementsByTagName("h1")[0].innerHTML=letters ;
-}
