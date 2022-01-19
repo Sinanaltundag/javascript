5ef37f61391c520391efb548e251fc9c0f816cf4
peykani <peykani@gmail.com>
2022-01-19 17:40:28 +0300

    number variables and methods

--- w3schools/ders1/script.js
+++ w3schools/ders1/script.js
@@ -25,7 +25,7 @@ function escseq() {
     document.getElementsByTagName("h2")[1].innerHTML=str.replace("are", "were") ;
  } */
 
- let sentence = "   when did you come?   ";
+/*  let sentence = "   when did you come?   ";
  let str2 = sentence.trim();// sağ sol boşlukları kaldırma
  
  function escseq3() {
@@ -37,4 +37,36 @@ function escseq() {
    let str5 = "What are you doing?";
 
    document.getElementById("h1").innerHTML=str5.split(" ")[2]; // stringleri bölerek array oluşturmak için
+} */
+
+function escseq() {
+  let int = 1234;
+
+  document.getElementById("h1").innerHTML =
+    int.toString() + " & " + "4321+11=" + (4321 + 11).toString(); // sayıları metine çevirme
 }
+function escseq2() {
+  let float = 3.36579;
+
+  document.getElementsByTagName("h2")[0].innerHTML = float.toFixed(2); //para yuvarlama gibi şeyler. noktadan sonrasını yuvarlıyor
+}
+function escseq3() {
+  let float = 3.36579;
+
+  document.getElementsByTagName("h2")[1].innerHTML = float.toPrecision(2); //tam yuvarlama
+}
+function escseq4() {
+   let float = "    3.36579   ";
+ 
+   document.getElementsByTagName("h2")[2].innerHTML = Number(float); //number yapma boşluklar ignore oluyor
+ }
+ function escseq5() {
+   let float = "3.96579 monety";
+ 
+   document.getElementsByTagName("h2")[3].innerHTML = parseInt(float); //metin içinden ilk sayı doğal sayı olur. önde sayı olmalı 
+ }
+ function escseq6() {
+   let float = "    -3.36579   45343 jvoıjfdva";
+ 
+   document.getElementsByTagName("h2")[4].innerHTML = parseFloat(float); //metin içinden ilk sayı ondalık sayı olur önde sayı olmalı 
+ }
