fbe370e921a76f46d3c99c8af9c4010111a0d911
peykani <peykani@gmail.com>
2022-01-20 23:19:52 +0300

    var let const

--- clarusway/ders3/script.js
+++ clarusway/ders3/script.js
@@ -1,27 +1,35 @@
-console.log("I am from file");
+// console.log("I am from file");
 
-let x =7;
-let y = 5;
+// var x =7;
+// var y = 5;
 
-console.log(x);
-console.log({y});
+// console.log(x);
+// console.log({y});
 
-console.log(typeof(y));
+// console.log(typeof(y));
 
-var a = "global"
-console.log(a);
-{
-    let a = "scope";
-    a="2. scope"
-    console.log(a);
-    {
-let a = "inner scope";
-console.log(a);
-a="scope";
-console.log(a);
+// var a = "global"
+// console.log(a);
+// {
+//     let a = "scope";
+//     a="2. scope"
+//     console.log(a);
+//     {
+// let a = "inner scope";
+// console.log(a);
+// a="scope";
+// console.log(a);
 
 
-}
-console.log(a);
-}
-console.log(a);
+// }
+// console.log(a);
+// }
+// console.log(a);
+
+var a = 30;
+let= 20;
+const h = 'constant variable';
+console.log(h);
+
+
+
