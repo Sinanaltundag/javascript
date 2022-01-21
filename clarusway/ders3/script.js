34b691ef2a4efa1225afef7f62ce1f92f7fc54a8
peykani <peykani@gmail.com>
2022-01-21 15:31:07 +0300

    variable types

--- clarusway/ders3/script.js
+++ clarusway/ders3/script.js
@@ -26,10 +26,52 @@
 // }
 // console.log(a);
 
-var a = 30;
-let= 20;
-const h = 'constant variable';
-console.log(h);
+// var a = 30;
+// let= 20;
+// const h = 'constant variable';
+// console.log(h);
 
 
 
+console.log(typeof 5.33);
+console.log(typeof 0);
+console.log(typeof typeof 5);
+console.log(typeof (3/2));
+console.log(typeof NaN);
+console.log(typeof (1/0));
+console.log(typeof Infinity);
+console.log(typeof function aa() {
+    
+});
+console.log(typeof 1);
+let inst = "sinan"
+let d =`merhaba ${inst}` //
+console.log(d);
+let e= 1+3;
+
+
+console.log(Boolean(NaN) );
+console.log(Boolean(Infinity));
+console.log(Boolean(null));
+console.log(Boolean(""));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(isNaN(e)));
+console.log(Boolean(3=="3"));
+
+console.log(Boolean("")); //false
+console.log(Boolean(3 < 5)); //true
+console.log(Boolean(3 > 5)); //false
+console.log(Boolean(3 == 5)); //false
+console.log(Boolean(0)); //false
+console.log(Boolean('0')); //true
+var s;
+console.log(Boolean(s)); //false undefined
+console.log(Boolean(null)); //false
+console.log(typeof s);
+console.log(typeof null);
+console.log(null == false);
+console.log(null == true);
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(2 == '2');
