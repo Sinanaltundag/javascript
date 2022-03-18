a8e09f32fd9ea66a7ab7c4ceb75fef70c0d11666
peykani <peykani@gmail.com>
2022-03-18 11:33:57 +0300

    make modul, import export

---
+++ clarusway/modulesson/index.js
@@ -0,0 +1,18 @@
+console.log('» index.js is running');
+
+
+import square, {WHW, WORKING_HOURS_WEEK, dec, inc,sum} from "./myModule.js"
+
+console.log(WHW)
+console.log( square(3) )
+
+inc(4);
+
+dec()
+
+console.log(dec())
+
+console.log(WORKING_HOURS_WEEK)
+
+console.log(sum(4,6))
+
